a8e23f41f3d8f25eb0148f0157dabb54f0c33d3c
monodesu <m451577581@live.com>
2023-10-21 00:02:03 +0900

    更新邮件部分

--- src/Mail.cs
+++ src/Mail.cs
@@ -1,38 +1,61 @@
-#pragma warning disable IDE0044 // 添加只读修饰符
-#pragma warning disable CS8602 // 解引用可能出现空引用。
-#pragma warning disable CS8604 // 解引用可能出现空引用。
-
 using System.Net;
 using System.Net.Mail;
 
-namespace KanonBot;
-public static class Mail
+namespace KanonBot
 {
-    private static Config.Base config = Config.inner!;
-    public class MailStruct
+    public static class Mail
     {
-        public required Arr<String> MailTo; //收件人，可添加多个
-        public Arr<String> MailCC = Empty; //抄送人，不建议添加
-        public required string Subject; //标题
-        public required string Body; //正文
-        public required bool IsBodyHtml;
-    }
-    public static void Send(MailStruct ms)
-    {
-        MailMessage message = new();
-        if (ms.MailTo.Length == 0) return;
-        ms.MailTo.Iter(s => message.To.Add(s)); //设置收件人
-        ms.MailCC.Iter(s => message.CC.Add(s)); //设置发件人
-        message.From = new MailAddress(config.mail.userName); //设置发件人
-        message.Subject = ms.Subject;
-        message.Body = ms.Body;
-        message.IsBodyHtml = ms.IsBodyHtml;
-        var client = new SmtpClient(config.mail.smtpHost, config.mail.smtpPort)
+        static Mail()
         {
-            Credentials = new System.Net.NetworkCredential(config.mail.userName, config.mail.passWord), //设置邮箱用户名与密码
-            EnableSsl = true //启用SSL
-        }; //设置邮件服务器
-        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-        client.Send(message); //发送
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        private static readonly Config.Base config = Config.inner!;
+
+        public class MailContent
+        {
+            public List<string> Recipients { get; } // 收件人列表
+            public List<string> CC { get; } // 抄送列表
+            public string Subject { get; }
+            public string Body { get; }
+            public bool IsBodyHtml { get; }
+
+            public MailContent(List<string> recipients, string subject, string body, bool isBodyHtml, List<string> cc = null)
+            {
+                if (recipients == null || recipients.Count == 0)
+                    throw new ArgumentException("Recipients cannot be empty.", nameof(recipients));
+
+                Recipients = recipients;
+                Subject = subject ?? throw new ArgumentNullException(nameof(subject));
+                Body = body ?? throw new ArgumentNullException(nameof(body));
+                IsBodyHtml = isBodyHtml;
+                CC = cc ?? new List<string>(); 
+            }
+        }
+
+        public static void Send(MailContent content)
+        {
+            if (content == null)
+                throw new ArgumentNullException(nameof(content));
+
+            using var message = new MailMessage
+            {
+                From = new MailAddress(config.mail!.userName!), // 设置发件人
+                Subject = content.Subject,
+                Body = content.Body,
+                IsBodyHtml = content.IsBodyHtml
+            };
+
+            content.Recipients.ForEach(recipient => message.To.Add(recipient));
+            content.CC.ForEach(cc => message.CC.Add(cc));
+
+            using var client = new SmtpClient(config.mail.smtpHost, config.mail.smtpPort)
+            {
+                Credentials = new NetworkCredential(config.mail.userName, config.mail.passWord),
+                EnableSsl = true
+            };
+
+            client.Send(message);
+        }
     }
 }
